2e3c192c118a2a476e20066f0eba3b425484b165
amir <amir.ahmady@gmail.com>
2020-01-08 17:07:58 -0500

    add functions for converting desire lead movement to pulse(step)

--- test.py
+++ test.py
@@ -20,21 +20,20 @@ def read_csv_file(filename="trajectory.csv"):
     return data
 
 
-def lead_per_pulse(stepping, leadScrewLead, leadScrewDimension='in'):
-    if leadScrewDimension.lower() == 'in':
-        lead = (leadScrewLead*25.4)/(200*stepping) # 200 is stepper deg
+def lead_per_pulse(stepping, leadScrewLead, unit='in'):
+    if unit.lower() == 'in':
+        lead = (leadScrewLead * 25.4) / (200 * stepping)  # 200 is stepper deg
     else:
         lead = 1
         pass
     return lead
 
 
-def move_by_mm(mm, lead = 4.9609375e-05):
-    return round(mm/lead)
+def move_by_mm(mm, lead=4.9609375e-05):
+    return round(mm / lead)
 
 
-
-def move_back_zoro(mouduleTMCM_1276, speed = 150000):
+def move_back_zoro(mouduleTMCM_1276, speed=150000):
     print("Moving back to 0")
     mouduleTMCM_1276.moveTo(0, speed)
     # Wait until position 0 is reached
@@ -43,10 +42,17 @@ def move_back_zoro(mouduleTMCM_1276, speed = 150000):
 
     print("Reached Position 0")
 
+def move_to_pos(mouduleTMCM_1276, position, unit='SI'):
+    # add in or si postioning
+    mouduleTMCM_1276.moveBy(move_by_mm(position))
+    mouduleTMCM_1276.getAxisParameter(mouduleTMCM_1276.APs.ActualPosition)
+    while not (mouduleTMCM_1276.positionReached()):
+        pass
+
 
 def main():
     PyTrinamic.showInfo()
-    print("PyTrinamic.showInfo()")
+    print("Preparing parameters")
     connectionManager = ConnectionManager(argList=['--interface', 'pcan_tmcl'])
     # myInterface = "pcan_tmcl"
     myInterface = connectionManager.connect()
@@ -55,46 +61,37 @@ def main():
     maxAcceleration = 100000
     DEFAULT_MOTOR = 0
     Stepping = 256
-    i = 0
-    CANBitrate = 200
-    print("Preparing parameters")
-    lead = lead_per_pulse(256,0.10,'in')
-    print(lead)
+    lead = lead_per_pulse(256, 0.10, 'in')
+
     mouduleTMCM_1276.setMaxAcceleration(maxAcceleration)
     mouduleTMCM_1276.setMaxVelocity(maxSpeed)
     mouduleTMCM_1276.setAxisParameter(mouduleTMCM_1276.APs.CurrentStepping, Stepping)
+    print('max speed is:', mouduleTMCM_1276.getMaxVelocity())
     print("Start position is:", mouduleTMCM_1276.getActualPosition())
-    # print(TMCM_1276.getGlobalParameter(CANBitrate,bank),CANBitrate,'***',bank)
-    moveBymm = move_by_mm(2.54)
-    print(moveBymm)
-    mouduleTMCM_1276.moveBy(moveBymm)
-    mouduleTMCM_1276.getAxisParameter(mouduleTMCM_1276.APs.ActualPosition)
-    while not (mouduleTMCM_1276.positionReached()):
-        pass
+    move_back_zoro(mouduleTMCM_1276)
 
-    # trajectoryFile = "trajectory.csv"
-    # trajectoryData = read_csv_file(trajectoryFile)
-    # start = time.time()
-    # i=0
-    # for item in trajectoryData[:100]:
-    #     i = i+1
-    #     print(i, item[0])
-    #     print(round(float(item[0])*10))
-    #     # TMCM_1276.moveBy(TMCM_1276.getActualPosition() + round(float(item[0])*10))
-    #     # TMCM_1276.getAxisParameter(TMCM_1276.APs.ActualPosition)
-    #     # while not (TMCM_1276.positionReached()):
-    #     #     pass
-    # end = time.time()
-    # print(end - start)
-    # # TMCM_1276.setAxisParameter(TMCM_1276.APs.CurrentStepping, 0)
-    # print("Moving back to 0")
-    # #TMCM_1276.moveTo(0, 100000)
-    #
-    # # Wait until position 0 is reached
-    # #while not (TMCM_1276.positionReached()):
-    #     #pass
+    # moveBymm = move_by_mm(0.1)
+    # print(moveBymm)
+    # mouduleTMCM_1276.moveBy(moveBymm)
+    # mouduleTMCM_1276.getAxisParameter(mouduleTMCM_1276.APs.ActualPosition)
+    # while not (mouduleTMCM_1276.positionReached()):
+    #     pass
+    # print("The position is:", mouduleTMCM_1276.getActualPosition())
+
+    trajectoryFile = "trajectory.csv"
+    trajectoryData = read_csv_file(trajectoryFile)
+    start = time.time()
+    curentPostion = 0
+    for item in trajectoryData[400:600]:
+        moveTo = float(item[0]) - curentPostion
+        move_to_pos(mouduleTMCM_1276, moveTo)
+        curentPostion = moveTo
+    end = time.time()
+    print(end - start)
+
     move_back_zoro(mouduleTMCM_1276)
     myInterface.close()
 
-if __name__== "__main__":
-  main()
+
+if __name__ == "__main__":
+    main()
